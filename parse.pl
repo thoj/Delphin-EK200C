9ffe5d80cd5d3f4ce2f5176fc4a56cd9e29fafa6
Thomas Jager <mail@jager.no>
2013-02-05 23:52:17 +0100

    more dumps

--- parse.pl
+++ parse.pl
@@ -16,8 +16,8 @@ while ( read(*STDIN, $header, $header_size) ) {
 		my (@t) = unpack("(VV)*", $data);
 		for (my $i = 0; $i < scalar @t; $i+=2) {
 			my $value = ($t[$i+1] >> 0) & (1 << 26) - 1;
-			my $chan = ($t[$i+1] >> 27) & (1 << 5) -1;
-			printf("%08x%08x %2d %9d %08x %8d %s\n", $t[$i],$t[$i+1], $chan, $t[$i], $value, $value, unpack("b32", pack("N", $t[$i+1])));
+			my $chan = ($t[$i+1] >> 27) & (1 << 6) -1;
+			printf("%08x%08x %2d %9d %08x %8d %s\n", $t[$i],$t[$i+1], $chan, $t[$i], $value, $value, unpack("B32", pack("N", $t[$i+1])));
 			$lines ++;
 		}
 	}
