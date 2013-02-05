7963a7eb5723a3bad17364d22164d122bb3c4298
Thomas Jager <mail@jager.no>
2013-02-05 19:29:56 +0100

    More info

--- parse.pl
+++ parse.pl
@@ -6,7 +6,7 @@ my $header;
 my $buf;
 my $data;
 my $i = 0;
-
+my $lines = 1;
 while ( read(*STDIN, $header, $header_size) ) {
 	$i ++;
 	my ($version, $code, $length, $ukn, $sequence) = unpack("nnNNN", $header);
@@ -15,18 +15,22 @@ while ( read(*STDIN, $header, $header_size) ) {
 		next if not $code == 128;
 #		my @f = unpack("(b18)*", $data);
 #		my @f = unpack("(h4)*", $data);
-		my (@t) = unpack("(Vf)*", $data);
+		my (@t) = unpack("(VV)*", $data);
 #		print @t, "\n";
 #		printf("%5d %2d %5d (%2d): ", $code, $length, $sequence, scalar @f);
-		for (my $i = 0; $i < scalar @t; $i+=2) {
-#			print $x;
+		for (my $i = 0; $i < scalar @t; $i+=2) {#			print $x;
 		#	printf("%20s", $x)
 #			printf("%10s ", unpack("h*",$x));
 #			printf("%10d", unpack("V", $x))
-			printf("%10d %15.1f ", $t[$i], $t[$i+1]);
+			my $flags = ($t[$i+1] >> 18) & (1 << 14) - 1;
+			my $value = ($t[$i+1] >> 0) & (1 << 18) - 1;
+#			printf("%.2f %d %d\n", $t[$i]/1000000, $value, $flags) if $flags == 3;
+			printf("%.2f %7d %7d %s\n", $t[$i]/1000000, $value, $flags, unpack("b32", pack("V", $t[$i+1])));
+#		print "\n";
+#			print "\n" if $lines % 5 == 0;
+			$lines ++;
 		}
 #		printf("%s", unpack("h".$length*2, $data));
-		print "\n";
 	}
 }
 
