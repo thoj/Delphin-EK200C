881a4132bc4049b87d323bb8e5acd43929586f8f
Thomas Jager <mail@jager.no>
2013-02-06 00:53:00 +0100

    conversion of 26bit signed to eng value

--- parse.pl
+++ parse.pl
@@ -7,6 +7,10 @@ my $buf;
 my $data;
 my $i = 0;
 my $lines = 1;
+
+my $range_max = 10000; #mV
+my $range_min = -10000; #mV
+
 while ( read(*STDIN, $header, $header_size) ) {
 	$i ++;
 	my ($version, $code, $length, $ukn, $sequence) = unpack("nnNNN", $header);
@@ -15,9 +19,14 @@ while ( read(*STDIN, $header, $header_size) ) {
 		next if not $code == 128;
 		my (@t) = unpack("(VV)*", $data);
 		for (my $i = 0; $i < scalar @t; $i+=2) {
-			my $value = ($t[$i+1] >> 0) & (1 << 26) - 1;
+			my $value = (($t[$i+1] >> 0) & (1 << 26) - 1);
+			if ($value > (2**25)) {
+				$value = -(($value - (2**26))/ 2**24) * ($range_min*1.02);
+			} else {
+				$value = ($value / 2**24) * ($range_max * 1.0343);
+			}
 			my $chan = ($t[$i+1] >> 27) & (1 << 6) -1;
-			printf("%08x%08x %2d %9d %08x %8d %s\n", $t[$i],$t[$i+1], $chan, $t[$i], $value, $value, unpack("B32", pack("N", $t[$i+1])));
+			printf("%2d %9d %8.2f\n", $chan, $t[$i], $value);
 			$lines ++;
 		}
 	}
